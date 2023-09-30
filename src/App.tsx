4b0b36a76c2b90a56486d054f9ddbf70a795cd04
millana_chotubaeva <chotubaevam@gmail.com>
2023-09-30 06:10:25 -0700

    added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+interface IFetchData {}
 function App() {
+  const URL = "https://api.unsplash.com/photos/?client_id=";
+  const KEY = "NOYQOOlALtaZBz9-rsFGN3HELTPDnSWQzrXp4G9i_Cg";
+  const [photos, setPhotos] = useState<object[]>([]);
+  const [reset, setReset] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Match game</h1>
     </div>
   );
 }
